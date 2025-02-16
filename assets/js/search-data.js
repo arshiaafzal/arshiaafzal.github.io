cc6c691ddc39f086d7c720826878787f5c5a00b1
arshiaafzal <68875155+arshiaafzal@users.noreply.github.com>
2025-02-16 18:32:37 +0000

    Deploying to gh-pages from @ arshiaafzal/arshiaafzal.github.io@9bc39a88a468ab9e9e3215abacc78a2201f0a869 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,16 +23,16 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+        },{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-rest-was-accepted-at-icml",
+          title: 'REST was accepted at ICML! 🤩',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
