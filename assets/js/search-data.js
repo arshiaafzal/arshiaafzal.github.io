97eef9fb985ba9344c356ab1d0e6be503712dfec
arshiaafzal <68875155+arshiaafzal@users.noreply.github.com>
2025-02-16 11:29:13 +0000

    Deploying to gh-pages from @ arshiaafzal/arshiaafzal.github.io@2502999d730b6b0e75d60d6d2ee05928cf44f051 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Here is my CV! You can also use the pdf download button to have the full pdf version :)",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
