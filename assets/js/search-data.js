390996b5fb474c036f8fffac62fe1aa5438af141
arshiaafzal <68875155+arshiaafzal@users.noreply.github.com>
2026-04-10 15:34:29 +0000

    Deploying to gh-pages from @ arshiaafzal/arshiaafzal.github.io@d6bf0f9700fa0c693344a3162f751f8ee422dcdf 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -38,7 +38,7 @@ ninja.data = [{
       section: "Posts",
       handler: () => {
         
-          window.location.href = "/blog/2026/attention-zoo/";
+          window.location.href = "/SSM_Story/";
         
       },
     },{id: "post-on-the-legacy-of-linear-transformers-in-positional-embeddings",
